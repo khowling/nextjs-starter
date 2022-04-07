1a16bc3e704adcc453422c02ced002581cc01a05
khowling <khowling@gmail.com>
2022-04-07 16:13:22 +0100

    add a function

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,36 @@
+import { useState } from 'react';
 import SmallCard from '../components/SmallCard';
 import { projectIcons } from '../components/Icons';
-
 import { projects } from '../utils/projectsData';
 
-const Home = () => (
-  <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        );
-      })}
+function Home() {
+  const [fromServer, setFromServer] = useState('');
+
+  async function callme() {
+    const respt = await fetch(`/api/MyOrders`).then((r) => r.text());
+    console.log(`reutrned ${respt}`);
+    setFromServer(respt);
+  }
+
+  return (
+    <div className="home">
+      <h1>What Can I Deploy to Static Apps?</h1>
+      <button onClick={callme}>Call API {fromServer}</button>
+      <div className="card-grid">
+        {projects.map((project) => {
+          const Icon = projectIcons[project.id];
+          return (
+            <SmallCard
+              key={project.id}
+              Icon={Icon}
+              title={project.name}
+              slug={project.slug}
+            />
+          );
+        })}
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Home;
